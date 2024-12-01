02257df98f75917e5cdf9d718c173030a74b0a3c
diegodisant <developerdiego0@gmail.com>
2024-12-01 16:37:10 -0600

    feat: finish calculations

--- src/main.c
+++ src/main.c
@@ -5,44 +5,44 @@
 #include "calc.h"
 
 int main() {
-  //printBanner();
+  printBanner();
 
   RocketParams rocketParams = {
-    0.924,  // L
-    0.05,   // D
-    0.046,  // DInt
-    1.555,  // mC
-    0.183,  // mP
+    2.4,  // L
+    0.134,   // D
+    0.128,  // DInt
+    48.2,  // mC
+    6.38,  // mP
   };
   Rocket *rocket = buildRocket(rocketParams);
 
   CalcParams calcParams = {
-    18,          // vH
-    327.794,     // Cs
-    1333,        // h
-    1864,        // Alt
-    0.9308,      // pH
-    0.000017065, // u0
-    5,           // v1
+    20.2,       // vH
+    332.529,     // Cs
+    2994,        // h
+    2000,        // Alt
+    1.00649,     // pH
+    0.0000174645,// u0
+    5.54,           // v1
     0.8,         // Cd
-    0.07,        // S
+    0.014,       // S
     2.5,         // n
     0.49,        // X1
     1.7,         // Cx
-    8,           // Nc
+    10,           // Nc
     1000,        // Csc
     222,         // Css
-    0.03,        // mcs
-    0.03,        // mcc
-    0.0366,      // mco
-    0.1,         // mp
-    332,         // Fmax
-    41,          // Mr
-    0.023,       // Dremp
-    0.175,       // mmsp
-    0.004,       // Thi
-    267.33,      // aparacaidas
-    0.41,        // posi
+    0.0272,      // mcs
+    0.00967,     // mcc
+    0.281,       // mco
+    3.36,       // mp
+    6750,        // Fmax
+    90,          // Mr
+    0.128,       // Dremp
+    4.92,        // mmsp
+    0.01,        // Thi
+    62.27,      // aparacaidas
+    0.33,        // posi
   };
   Calc *calc = buildCalc(rocket, calcParams);
 
